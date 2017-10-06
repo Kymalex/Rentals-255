99944e353e9485e4bdb3398bcbbfd219045902b3
Victor Kariuki <karizvic@gmail.com>
2017-10-06 09:29:53 +0300

    inital commit

---
+++ css/exe.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main()
+{
+	int a, b, result;
+	printf("Enter the first number: \n");
+	scanf("%d", & a);
+	printf("Enter the second number: \n");
+	scanf("%d", & b);
+	dividing = a / b ;
+	result = (a/b);
+	printf("result")
+
+	if (a ==0 or b == 0)
+	{
+		return dividing;
+	}
+}
---
+++ css/rose.py
@@ -0,0 +1,11 @@
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+dividing = a / b
+if a == 3 or b == 3:
+	print("None can be a three")
+	a = input("Enter the first number: ")
+	b = input("Enter the second number: ")
+	print(dividing)
+else:
+	print(dividing)
+
